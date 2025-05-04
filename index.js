a9c002b0f22950836e3a05c0f424cf120f7526c5
MD. Mehedi Hasan Tonmoy <107616839+tonmoy3673@users.noreply.github.com>
2025-05-04 16:46:59 +0600

    :bulb: setup and run the server

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 require('dotenv').config();
 const express = require('express');
+const cookieParser = require('cookie-parser')
+const cors = require('cors');
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+// ========= middleWare =========//
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    origin:'http://localhost:3000',
+    credentials:true
+}));
+
+app.get('/', (req, res) => {
+    res.send('Server is Running')
+  })
+
+app.listen(port,()=>console.log(`Server is running on port ${port}`))
